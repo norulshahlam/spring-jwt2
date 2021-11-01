655b2b810ca542840486971519a32446b8b088df
Norulshahlam <norulshahlam@gmail.com>
2021-11-01 23:27:23 +0800

    s

--- src/main/java/shah/springjwt2/jwt/AccessTokenGenerator.java
+++ src/main/java/shah/springjwt2/jwt/AccessTokenGenerator.java
@@ -21,11 +21,8 @@ public class AccessTokenGenerator {
         String access_token = JWT.create().withSubject(user.getUsername()).withExpiresAt(ACCESS_TOKEN_VALIDITY)
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles",
-                        user.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-                .collect(Collectors.toList()))
-                .withClaim("Owner", "Norulshahlam")
-                .sign(ALGORITHM);
+                        user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
+                .withClaim("Owner", "Norulshahlam").sign(ALGORITHM);
         return access_token;
     }
-
 }
