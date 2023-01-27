f891263b42e54023caa911f437ad49602e4afc43
norulshahlam <norulshahlam@gmail.com>
2023-01-27 10:56:44 +0800

    ss

--- src/main/java/shah/springjwt2/constant/Constants.java
+++ src/main/java/shah/springjwt2/constant/Constants.java
@@ -8,7 +8,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 public class Constants {
 
     public static final String JWT_SECRET="secret";
-    public static final Date ACCESS_TOKEN_VALIDITY=new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(1));
+    public static final Date ACCESS_TOKEN_VALIDITY=new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(1));
     public static final Date REFRESH_TOKEN_VALIDITY=new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(1));
     public static final Algorithm ALGORITHM= Algorithm.HMAC256(JWT_SECRET.getBytes());
 }
